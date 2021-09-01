a82772b1faa200ab50640ae46f0911c462eab3b4
sardiimre90 <sardiimre90@gmail.com>
2021-09-01 19:13:38 +0200

    add min method

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,33 @@ import java.util.Random;
 
 public class Main {
 
+    static int[] array = new int[20];
+
+
     public static void main(String[] args) {
 
-        int[] array = new int[20];
+        fillArray();
+        System.out.println("----------------");
+        min();
 
-        for(int i = 0; i <  array.length; i++) {
-            array[i] = (int)(Math.random() * 100);
-            System.out.print(array[i] + ", ");
+        }
+
+    public static int[] fillArray() {
+    for (int i = 0; i < array.length; i++) {
+        array[i] = (int) (Math.random() * 100);
+        System.out.println(array[i] + ", ");
+    }
+        return array;
+    }
+    public static int min() {
+
+        int min = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                System.out.println("A legkisebb: " +min);
+                }
+            } return min;
         }
     }
-}
